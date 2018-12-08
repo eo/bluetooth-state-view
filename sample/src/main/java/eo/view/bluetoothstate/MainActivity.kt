0124c8286516ae11766961d64c7ac5d14dc9c6d5
Erdem Orman <ormanerdem@gmail.com>
2018-12-08 17:41:19 +0300

    Sample is updated to show both rounded and sharp versions

--- sample/src/main/java/eo/view/bluetoothstate/MainActivity.kt
+++ sample/src/main/java/eo/view/bluetoothstate/MainActivity.kt
@@ -2,6 +2,8 @@ package eo.view.bluetoothstate
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
@@ -13,24 +15,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupConnectingIcon()
-        setupSearchingIcon()
+        setupConnectingAnimations()
+        setupSearchingAnimations()
     }
 
-    private fun setupConnectingIcon() {
-        val avd = AnimatedVectorDrawableCompat.create(this,
-            R.drawable.ic_bluetooth_connecting_rounded)
-        connectingImageView.setImageDrawable(avd)
+    private fun setupConnectingAnimations() {
+        setInfiniteAnimatedVectorDrawable(
+            roundedConnectingImageView,
+            R.drawable.ic_bluetooth_connecting_rounded
+        )
+
+        setInfiniteAnimatedVectorDrawable(
+            sharpConnectingImageView,
+            R.drawable.ic_bluetooth_connecting_sharp
+        )
+    }
+
+    private fun setupSearchingAnimations() {
+        setInfiniteAnimatedVectorDrawable(
+            roundedSearchingImageView,
+            R.drawable.ic_bluetooth_searching_rounded
+        )
+
+        setInfiniteAnimatedVectorDrawable(
+            sharpSearchingImageView,
+            R.drawable.ic_bluetooth_searching_sharp
+        )
+    }
+
+    private fun setInfiniteAnimatedVectorDrawable(
+        imageView: ImageView,
+        @DrawableRes avdResource: Int
+    ) {
+        val avd = AnimatedVectorDrawableCompat.create(this, avdResource)
+        imageView.setImageDrawable(avd)
 
         avd?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
             override fun onAnimationEnd(drawable: Drawable?) {
-                connectingImageView.post { avd.start() }
+                imageView.post { avd.start() }
             }
         })
         avd?.start()
-    }
-
-    private fun setupSearchingIcon() {
-        searchingImageView.setImageResource(R.drawable.ic_bluetooth_search_rounded)
     }
 }
