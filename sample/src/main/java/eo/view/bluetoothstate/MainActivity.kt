15d33007b058ad03c1ad964c7f3d2ff6780a6ce8
Erdem Orman <ormanerdem@gmail.com>
2018-12-08 11:21:29 +0300

    Sample is updated to contain Bluetooth connecting and search icons

--- sample/src/main/java/eo/view/bluetoothstate/MainActivity.kt
+++ sample/src/main/java/eo/view/bluetoothstate/MainActivity.kt
@@ -1,12 +1,36 @@
 package eo.view.bluetoothstate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupConnectingIcon()
+        setupSearchingIcon()
+    }
+
+    private fun setupConnectingIcon() {
+        val avd = AnimatedVectorDrawableCompat.create(this,
+            R.drawable.ic_bluetooth_connecting_rounded)
+        connectingImageView.setImageDrawable(avd)
+
+        avd?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                connectingImageView.post { avd.start() }
+            }
+        })
+        avd?.start()
+    }
+
+    private fun setupSearchingIcon() {
+        searchingImageView.setImageResource(R.drawable.ic_bluetooth_search_rounded)
     }
 }
